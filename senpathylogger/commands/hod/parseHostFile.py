420321808bf40e636acfccf5fc1e6f19637dcd33
pgsenpathy <cs15s041@smail.iitm.ac.in>
2016-08-04 16:38:55 +0530

    Add exception error for hostname: Now a mischief in the hostfile will be reported

--- senpathylogger/commands/hod/parseHostFile.py
+++ senpathylogger/commands/hod/parseHostFile.py
@@ -1,9 +1,16 @@
+import sys
+
 def parseHostFile(hostFileName):
     hosts = {}
     with open(hostFileName) as f:
         for line in f:
             a = line.split()
-            dept = a[0]
-            hostname = a[1]
-            hosts[a[1]] = a[0]
+            try:
+                dept = a[0]
+                hostname = a[1]
+            except IndexError as e:
+                sys.exit("Note: Your hostnames file has some issues\n" + \
+                         "\t\tThis could be possible due to empty lines\n" + \
+                         "\t\tOr due to missing hostname or departname")
+            hosts[hostname] = dept
     return hosts
